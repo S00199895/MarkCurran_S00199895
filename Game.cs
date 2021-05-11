b5f2a0f75a5a0ba2ea0768261dc7265cbf48b644
Mark Curran <S00199895@mail.itsligo.ie>
2021-05-11 09:38:28 +0100

    created game class

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkCurran_S00199895
+{
+	class Game
+	{
+		public string Name { get; set; }
+		public int CriticScore { get; set; }
+		public string Description { get; set; }
+		public string Platform { get; set; }
+		public decimal Price { get; set; }
+		public string GameImage { get; set; }
+	}
+}
